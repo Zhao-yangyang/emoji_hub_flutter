de4a889cfe9fa5b6e7ab47e55c1e85ed2dfa12d5
sheep <946719919@qq.com>
2024-12-25 19:26:59 +0800

    Remove unnecessary import from floating_window_screen.dart to improve code clarity and maintainability.

--- lib/ui/screens/floating_window_screen.dart
+++ lib/ui/screens/floating_window_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import '../../core/theme/app_theme.dart';
 import 'dart:io';
 import '../../data/repositories/emoji_repository.dart';
